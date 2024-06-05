36b9519558f76714f80477a753c5dace59337e00
TymofiiPys <nfstimoha2004@gmail.com>
2024-06-05 22:19:56 +0300

    add skeleton to PascalLexer, integrate lombok

---
+++ src/main/java/org/pascallexer/PascalLexer.java
@@ -0,0 +1,27 @@
+package org.pascallexer;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class PascalLexer
+{
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            runFile(args[0]);
+        }  else {
+            System.out.println("Supply a pascal code filename");
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void run(String source) {
+
+    }
+}
